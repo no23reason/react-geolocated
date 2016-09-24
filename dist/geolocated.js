62bf5a307c0b3a1258e4a02cd766e66046356180
Dan Homola <dan.homola@hotmail.cz>
2016-09-24 16:48:01 +0200

    Update dist

--- dist/geolocated.js
+++ dist/geolocated.js
@@ -75,6 +75,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	});
 	exports.geoPropTypes = undefined;
 	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
 	var _react = __webpack_require__(2);
@@ -113,7 +115,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            function Geolocated(props) {
 	                _classCallCheck(this, Geolocated);
 	
-	                var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Geolocated).call(this, props));
+	                var _this = _possibleConstructorReturn(this, (Geolocated.__proto__ || Object.getPrototypeOf(Geolocated)).call(this, props));
 	
 	                _this.state = {
 	                    coords: null,
@@ -179,7 +181,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            }, {
 	                key: 'render',
 	                value: function render() {
-	                    return _react2.default.createElement(WrappedComponent, this.state);
+	                    return _react2.default.createElement(WrappedComponent, _extends({}, this.state, this.props));
 	                }
 	            }]);
 	
