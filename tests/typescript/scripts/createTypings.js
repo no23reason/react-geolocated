c52970c566d16f21b1fbd341a2ebf17479908f39
Dan Homola <dan.homola@hotmail.cz>
2017-08-19 21:33:30 +0200

    chore: make the createTypings Node 4 compatible

--- tests/typescript/scripts/createTypings.js
+++ tests/typescript/scripts/createTypings.js
@@ -1,12 +1,12 @@
-const { readFileSync, writeFileSync } = require('fs');
+const fs = require('fs');
 
 // does what typings package used to do
 const main = () => {
-  const typings = readFileSync('../../index.d.ts', { encoding: 'utf8' });
+  const typings = fs.readFileSync('../../index.d.ts', { encoding: 'utf8' });
   const wrapped = `declare module 'react-geolocated' {
       ${typings}
       }`;
-  writeFileSync('./index.d.ts', wrapped, { encoding: 'utf8' });
+  fs.writeFileSync('./index.d.ts', wrapped, { encoding: 'utf8' });
 };
 
 main();
