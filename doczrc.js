7983a65be7050eccae77049fe3dd0340f345e446
Dan Homola <dan.homola@hotmail.cz>
2019-09-08 16:47:06 +0200

    docs: upgrade docz

--- doczrc.js
+++ doczrc.js
@@ -1,10 +1,9 @@
 module.exports = {
     base: "/react-geolocated/",
-    hashRouter: true,
     ignore: [
-        "changelog.md",
-        "code_of_conduct.md",
-        "contributing.md",
-        "license.md",
+        "CHANGELOG.md",
+        "CODE_OF_CONDUCT.md",
+        "CONTRIBUTING.md",
+        "LICENSE",
     ],
 };
